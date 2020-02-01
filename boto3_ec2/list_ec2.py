6edd78915821248147ab5943081edd8a5b19eb79
Somanath Patil <patilsomnath86@gmail.com>
2020-01-31 23:58:14 -0800

    list ec2 instances ( running, terminated,pending )

--- boto3_ec2/list_ec2.py
+++ boto3_ec2/list_ec2.py
@@ -8,3 +8,4 @@ ec2 = boto3.resource('ec2')
 for instance in ec2.instances.all():
     #if instance.state['Name'] == 'stopped':
          print("Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState: {5}\n".format(instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state))
+        
