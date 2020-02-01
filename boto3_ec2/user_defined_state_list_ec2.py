69616e1483a2f3c86eb8d8c502b4329d01acdf5f
Somanath Patil <patilsomnath86@gmail.com>
2020-01-31 23:59:01 -0800

    list Ec2 instances based used defined state

--- boto3_ec2/user_defined_state_list_ec2.py
+++ boto3_ec2/user_defined_state_list_ec2.py
@@ -12,3 +12,4 @@ print(state)
 for instance in ec2.instances.all():
     if instance.state['Name'] == state:
          print("Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState: {5}\n".format(instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state))
+            
